309759339121a867343407e0bb282358fae995c5
sajal-19 <91428253+sajal-19@users.noreply.github.com>
2023-11-28 21:39:13 +0530

    Update Image.jsx

--- client1/src/Image.jsx
+++ client1/src/Image.jsx
@@ -1,7 +1,7 @@
 export default function Image({src,...rest}) {
     src = src && src.includes('https://')
       ? src
-      : 'http://localhost:4000/uploads/'+src;
+      : 'https://homily-api1.vercel.app/uploads/'+src;
     return (
       <img {...rest} src={src} alt={''} />
     );
