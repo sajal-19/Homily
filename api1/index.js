1617502fc8445e7bbbfb03bc07c890ae09453760
sajal-19 <91428253+sajal-19@users.noreply.github.com>
2023-11-28 22:19:50 +0530

    Update index.js

--- api1/index.js
+++ api1/index.js
@@ -24,7 +24,7 @@ app.use('/uploads', express.static(__dirname+'/uploads'));
 
 app.use(
   cors({
-    origin: process.env.NODE_ENV === 'development' ? '*' : 'https://homily-frontend.vercel.app',
+    origin: 'https://homily-frontend.vercel.app',
     credentials: true,
     methods: ["POST", "GET"]
   })
